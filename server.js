c569a72745aa0783c59216da1a60cd61b385dc00
Macbie <kongzhengho@Kongs-MacBook-Air.local>
2020-04-18 04:02:03 +0800

    Fixed react routing bugs 1.2

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.listen(PORT, function() {
 });
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build/index.html'));
     });

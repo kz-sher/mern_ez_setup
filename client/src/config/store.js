3d17d2efa06335289973bfad31c6d2fb9b946934
Macbie <kongzhengho@Kongs-MacBook-Air.local>
2020-04-18 00:39:39 +0800

    Fixed bugs 1.1

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistReducer } from 'redux-persist';
 import { persistConfig } from './persist';
 import reducers from 'reducers';
 import CrossTabListener from 'utils/CrossTabListener';
 
 const persistedReducer = persistReducer(persistConfig, reducers);
-const store = createStore(persistedReducer, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+// const store = createStore(persistedReducer, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+const store = createStore(persistedReducer, {}, applyMiddleware(reduxThunk));
 
 // Add listener for local storage change for state persistence across multi-tabs
 window.addEventListener('storage', CrossTabListener(store, persistConfig));
